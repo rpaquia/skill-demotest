c3068ea321f430c63dbdcb5890e447ba73369e92
rpaquia <97699019+rpaquia@users.noreply.github.com>
2022-02-04 15:07:27 -0800

    Update SkillDemoTestTest.java

--- SkillDemoTestTest.java
+++ SkillDemoTestTest.java
@@ -11,7 +11,9 @@ public class SkillDemoTestTest {
 }
 //javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
 //scp -r . cs15lwi22@ieng6.ucsd.edu:~/markdown-parse
-//scp -r . cs15lwi22@ieng6.ucsd.edu:~/markdown-parse
+//scp -r . cs15lwi22aqj@ieng6.ucsd.edu:~/lib
 //java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
 //java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
 
+//$ vim SkillDemoTestTest.java (allows you to edit in terminal off remote server
+//write :wq to save onto remote server from the editor
