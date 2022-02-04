8456b96f3d0b771773995bcdbb9c0253d11fc79c
rpaquia <97699019+rpaquia@users.noreply.github.com>
2022-02-04 12:51:54 -0800

    Update SkillDemoTestTest.java

--- SkillDemoTestTest.java
+++ SkillDemoTestTest.java
@@ -4,8 +4,14 @@ import org.junit.*;
 public class SkillDemoTestTest {
     @Test
     public void multiplication() {
-        assertEquals(6, SkillDemoTest.multiply(2, 3));
+        assertEquals(5, SkillDemoTest.multiply(2, 3));
     }
     //Need to think about the test + commands
     //Checking Week 4
 }
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+//scp -r . cs15lwi22@ieng6.ucsd.edu:~/markdown-parse
+//scp -r . cs15lwi22@ieng6.ucsd.edu:~/markdown-parse
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
